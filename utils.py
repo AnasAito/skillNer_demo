2cca7f9f7f1bf5465428523f4d49adb45ceb5860
AnasAito <khdimaty@gmail.com>
2021-08-20 22:58:57 +0100

    add spacy

--- utils.py
+++ utils.py
@@ -32,7 +32,8 @@ def load_skill_extractor():
     try:
         nlp = spacy.load("en_core_web_lg")
     except:
-        os.system('ls')
+
+        os.system('pip install -U spacy')
         os.system("python -m spacy download en_core_web_lg")
         nlp = spacy.load("en_core_web_lg")
 
